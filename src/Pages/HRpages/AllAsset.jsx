0435e41b4a39e391c4d6b933f12d82b41df5a10f
farahsamsi <1827034farahsamsiprome@gmail.com>
2025-01-20 15:19:11 +0600

    added update and delete asset

--- src/Pages/HRpages/AllAsset.jsx
+++ src/Pages/HRpages/AllAsset.jsx
@@ -4,14 +4,71 @@ import { MdDelete, MdEdit, MdSearch } from "react-icons/md";
 import useAssets from "../../Hooks/useAssets";
 import { useState } from "react";
 import { BiSearch } from "react-icons/bi";
+import useAxiosSecure from "../../Hooks/useAxiosSecure";
+import Swal from "sweetalert2";
+import { FaFile } from "react-icons/fa";
 
 const AllAsset = () => {
   const [sort, setSort] = useState(false);
   const [search, setSearch] = useState("");
   const [filter, setFilter] = useState("");
-  // console.log(filter);
+  const [updateAsset, setUpdateAsset] = useState({});
 
-  const [assets] = useAssets(sort, search, filter);
+  const axiosSecure = useAxiosSecure();
+
+  const [assets, refetch] = useAssets(sort, search, filter);
+
+  const handleDelete = async (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        const res = await axiosSecure.delete(`/asset/${id}`);
+        if (res.data.deletedCount > 0) {
+          refetch();
+          Swal.fire({
+            title: "Deleted!",
+            text: "Your asset has been deleted.",
+            icon: "success",
+          });
+        }
+      }
+    });
+  };
+
+  const handleUpdate = async (asset) => {
+    document.getElementById(asset._id).showModal();
+    setUpdateAsset(asset);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const assetQuantity = parseInt(form.assetQuantity.value);
+
+    console.log("update for", updateAsset?.assetName);
+
+    const res = await axiosSecure.patch(`/updateAsset/${updateAsset._id}`, {
+      assetQuantity,
+    });
+    if (res.data.modifiedCount > 0) {
+      refetch();
+      document.getElementById(updateAsset._id).close();
+      Swal.fire({
+        position: "top-end",
+        icon: "success",
+        title: "Successfully Updated",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    }
+  };
 
   return (
     <section className="pb-9 w-11/12 mx-auto">
@@ -100,15 +157,60 @@ const AllAsset = () => {
 
                     <th>
                       <button
-                        // onClick={() => removeFromTeam(user.email)}
+                        onClick={() => handleUpdate(asset)}
                         className="btn btn-outline  text-xl"
                       >
                         <MdEdit className="text-yellow-400 text-3xl" />
                       </button>
+                      {/* You can open the modal using document.getElementById('ID').showModal() method */}
+                      <dialog id={asset._id} className="modal">
+                        <div className="modal-box">
+                          <form method="dialog">
+                            {/* if there is a button in form, it will close the modal */}
+                            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+                              ✕
+                            </button>
+                          </form>
+                          <div>
+                            <div className="card bg-base-100 w-full  shrink-0 shadow-2xl">
+                              <form
+                                onSubmit={handleSubmit}
+                                className="card-body"
+                              >
+                                <h1 className="text-center">
+                                  Update {asset?.assetName}
+                                </h1>
+                                <div className="form-control">
+                                  <label className="label">
+                                    <span className="label-text">Quantity</span>
+                                  </label>
+                                  <input
+                                    defaultValue={asset?.assetQuantity}
+                                    type="number"
+                                    name="assetQuantity"
+                                    placeholder="Enter quantity"
+                                    className="input input-bordered"
+                                  />
+                                </div>
+
+                                <div className="form-control mt-6">
+                                  <button
+                                    type="submit"
+                                    className="btn btn-primary text-white"
+                                  >
+                                    <FaFile />
+                                    Update
+                                  </button>
+                                </div>
+                              </form>
+                            </div>
+                          </div>
+                        </div>
+                      </dialog>
                     </th>
                     <th>
                       <button
-                        // onClick={() => removeFromTeam(user.email)}
+                        onClick={() => handleDelete(asset?._id)}
                         className="btn btn-outline  text-xl"
                       >
                         <MdDelete className="text-red-400 text-3xl" />
